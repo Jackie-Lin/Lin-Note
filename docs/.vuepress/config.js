1de8c851cfe43694d179dbe92069cbdbe50dae8a
JK_Lin <1351123861@qq.com>
2022-03-05 12:29:16 +0800

    测试 自动化部署

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,6 +1,15 @@
 // .vuepress/config.js
 module.exports = {
+    title:'Lin-Note',
+    base:'/Lin-Note/',
     themeConfig: {
+      nav:[{
+        text: 'Home', link: '/'
+      },{
+        text: 'GitHub', link: 'https://github.com/Jackie-Lin/Lin-UI/tree/master'
+      },{
+        text: 'Npm', link: 'https://www.npmjs.com/package/lin-ui2'
+      }],
       sidebar: [
         '/',
         '/note/deployEnvironment'
